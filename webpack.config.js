9513af1de5cf649562d38db3f0d874b6a0cfb937
应元东 <ying@yingDev.com>
2016-10-24 11:57:58 +0800

    webpack dedupe

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var webpack = require('webpack');
+
 module.exports = {
     entry: "./test.js",
     output: {
@@ -7,6 +9,15 @@ module.exports = {
     devtool: "source-map",
     module: {
         loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules, build)/,
+                loader: 'babel',
+	            query: {
+		            presets: ['es2015']
+	            }
+            }
         ]
-    }
+    },
+	plugins:[new webpack.optimize.DedupePlugin()]
 };
