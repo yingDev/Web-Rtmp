cbe0d339a5c006a1b77f9a7e91ea6199cbffaf43
Ying Yuandong <897326609@qq.com>
2016-10-24 23:02:28 +0800

    fix loop

--- misc/do-rtmpsuck.sh
+++ misc/do-rtmpsuck.sh
@@ -6,13 +6,13 @@ sudo sysctl -w net.inet.ip.forwarding=1
 # apply the pf rules:
 echo '
 rdr pass log on lo0 proto tcp from en0 to any port 1935 -> 127.0.0.1
-pass out on en0 route-to lo0 inet proto tcp from en0 to any port 1935 keep state
+pass out on en0 route-to lo0 inet proto tcp from en0 to any port 1935 keep state user != root
 ' | sudo pfctl -ef -
 
 # check the pf rules:
 # sudo pfctl -s all
 
-rtmpsuck -z;
+sudo rtmpsuck $@;
 
 # clear the pf rules:
 sudo pfctl -F all -f /etc/pf.conf
