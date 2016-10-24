8feba615c5e79b95172b59cb209bc386fdaef691
Ying Yuandong <897326609@qq.com>
2016-10-24 22:45:05 +0800

    rename

---
+++ misc/do-rtmpsuck.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# enable internal port forwarding:
+sudo sysctl -w net.inet.ip.forwarding=1
+
+# apply the pf rules:
+echo '
+rdr pass log on lo0 proto tcp from en0 to any port 1935 -> 127.0.0.1
+pass out on en0 route-to lo0 inet proto tcp from en0 to any port 1935 keep state
+' | sudo pfctl -ef -
+
+# check the pf rules:
+# sudo pfctl -s all
+
+rtmpsuck -z;
+
+# clear the pf rules:
+sudo pfctl -F all -f /etc/pf.conf
