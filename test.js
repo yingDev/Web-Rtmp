758d0583358ad034c5ce10126841b29a27bbc815
应元东 <ying@yingDev.com>
2016-10-31 12:46:14 +0800

    add alert

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const WebRtmpPlayer = require('./WebRtmpPlayer');
 
 //note: tcUrl是原始rtmp地址，不含流名称。参见rtmp spec
+alert('请在test.js中设置你的 tcUrl 参数');
 var player = new WebRtmpPlayer('ws://127.0.0.1:1999', 'live', 'B011', 'rtmp://<rtmp_server>/<app>');
 player.canvas.style['height'] = '100%';
 document.getElementById("vidCont").appendChild(player.canvas);
